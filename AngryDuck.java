0e57ca3fce342ef5b7ca993178f4ca72badfe768
Amir <amirmohd580@gmail.com>
2020-07-20 09:57:23 -0700

    Just added David's code to AngryDuck.
    Added David's Code so everything works fine

--- AngryDuck.java
+++ AngryDuck.java
@@ -1,11 +1,13 @@
-public class AngryDuck extends WaterFowl implements Teleport{
+public class AngryDuck extends WaterFowl implements Teleport,Attacker {
 
-     public AngryDuck(String name, String primaryColor) {
+     protected int killCount;
+
+     public AngryDuck(String name, String primaryColor, int killCount) {
           super(name, primaryColor);
           super.name = name;
           super.primaryColor = primaryColor;
+          this.killCount = killCount;
      }
-
 
      public void teleport() {
           X = (int)(( Math.random() * (999 - 501 + 1) + 501));
@@ -16,5 +18,24 @@ public class AngryDuck extends WaterFowl implements Teleport{
      public String getPosition()
      {
        return getPosition;
+     }
+
+     public int getKillCount()
+     {
+          return killCount;
+     }
+
+     public void attacker(){
+          int objectHealth = health;
+          if(!(name.equals("AngryDuck")) && health>0 && objectHealth>0){
+               if(objectHealth==1){
+                    this.killCount++;
+               }
+               objectHealth = health-1;
+               System.out.println("AngryDuck attack " + name + " " + this.killCount + " times.");
+          }
+          else{
+               System.out.println("AngryDuck can't attack " + name +".");
+          }
      }
 }
