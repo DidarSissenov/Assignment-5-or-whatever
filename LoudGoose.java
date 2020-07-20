a971448e2d08fb66ac0f19d1fe79081984e4830b
Ji Zhang <67984715+19981998zj@users.noreply.github.com>
2020-07-20 15:32:21 -0700

    Add files via upload
    Attacker interface for LoudGoose

--- LoudGoose.java
+++ LoudGoose.java
@@ -1,12 +1,33 @@
-public class LoudGoose extends WaterFowl {
-
-     public LoudGoose(String name, String primaryColor){
-          super(name, primaryColor);
-          super.name = name;
-          super.primaryColor = primaryColor;
-     }
-
-
-
-
+/**
+ * Auto Generated Java Class.
+ */
+public class LoudGoose extends WaterFowl implements Attacker{
+  
+  protected int killCount;
+  
+  public LoudGoose(String name, String primaryColor, int killCount){
+    super(name, primaryColor);
+    super.name=name;
+    super.primaryColor=primaryColor;
+    this.killCount=killCount;
+  }
+  
+  public int getKillCount(){
+    return killCount;
+  }
+  
+  public void attacker(){
+    int objectHealth = health;
+    if(!(name.equals("LoudGoose")) && health>0 && objectHealth>0){
+      if(objectHealt==1){
+        this.killCount++;
+      }
+      objectHealth = objectHealth-1;
+      System.out.println("LoudGoose attack " + name + " " + this.killCount + " times.");
+    }
+    else{
+      System.out.println("LoudGoose can't attack " + name + ".");
+    }
+  }
+  
 }
